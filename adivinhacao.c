d84d714a2f15aeb20d759aad3b834a93c34dcded
G.i.o <35783657+giosilvalmeida@users.noreply.github.com>
2025-05-05 17:41:12 +0100

    a

---
+++ adivinhacao.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    printf("********************************");
+    printf("Bem Vindo ao Jogo da Adivinhacao");
+    printf("********************************");
+}
