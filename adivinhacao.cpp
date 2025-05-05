1aef81116ac38f1f5094fddeb051d76ad855d795
G.i.o <35783657+giosilvalmeida@users.noreply.github.com>
2025-05-05 21:03:16 +0100

    Add files via upload
    I Changed this file to extention .cpp for compilation in the VSCODE
    I Aditioned plus lines in the code

---
+++ adivinhacao.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main(){
+
+    
+    printf("********************************");
+    printf("Bem Vindo ao Jogo da Adivinhacao");
+    printf("********************************");
+
+    int numerosecreto;
+    numerosecreto = 42;
+
+}
